5534241000d24c3479ceb1f3fed047444bc383d5
Enzo <enzonms12@gmail.com>
2025-05-17 10:57:05 -0300

    Fix: alter quantidade e remover funcionando

--- src/main/java/rattatoulie/demo/controllers/UserController.java
+++ src/main/java/rattatoulie/demo/controllers/UserController.java
@@ -1,9 +1,12 @@
 package rattatoulie.demo.controllers;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,5 +60,11 @@ public class UserController {
         user.finalizarCompra();
         userRepository.save(user);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        List<UserModel> users = userRepository.findAll()    ;
+        return ResponseEntity.ok(users);
     }
 }
